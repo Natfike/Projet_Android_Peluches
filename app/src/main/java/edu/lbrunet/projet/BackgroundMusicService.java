c3b48e8ea48873702d741a0c5f1c3c31b4db4b57
Natfik <69470725+Natfike@users.noreply.github.com>
2023-04-01 18:01:38 +0200

    file musicService

---
+++ app/src/main/java/edu/lbrunet/projet/BackgroundMusicService.java
@@ -0,0 +1,42 @@
+package edu.lbrunet.projet
+
+import android.app.Service;
+import android.content.Intent;
+import android.media.MediaPlayer;
+import android.os.IBinder;
+import android.provider.MediaStore;
+import android.util.Log;
+
+import androidx.annotation.Nullable;
+
+public class BackgroundMusicService extends Service {
+
+    MediaPlayer mediaPlayer;
+
+    @Nullable
+    @Override
+    public IBinder onBind(Intent intent) {
+        return null;
+    }
+
+    @Override
+    public int onStartCommand(Intent intent, int flags, int startId){
+        Log.d("mylog", "Started playing");
+        mediaPlayer = MediaPlayer.create(this, R.raw.married_life);
+        mediaPlayer.start();
+        return super.onStartCommand(intent,flags,startId);
+    }
+
+    @Override
+    public boolean stopService(Intent name){
+        return super.stopService(name);
+    }
+
+    @Override
+    public void onDestroy(){
+        super.onDestroy();
+        mediaPlayer.stop();
+        mediaPlayer.release();
+        mediaPlayer = null;
+    }
+}
